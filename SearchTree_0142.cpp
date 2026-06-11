bde6d7c8ec5f5ecd057b460232fa56ba654e0178
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-06-11 11:29:36 +0700

    membuat switch case

--- SearchTree_0142.cpp
+++ SearchTree_0142.cpp
@@ -142,5 +142,12 @@ int main()
         cout << "\nEnter your choice (1-5): ";
 
         char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch(ch)
+        {
+        case '1':
     }
 }
