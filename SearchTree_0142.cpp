009bafea96157929b35ba9152dc8cedd6757f48e
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-06-11 11:09:40 +0700

    selesai membuat void search

--- SearchTree_0142.cpp
+++ SearchTree_0142.cpp
@@ -6,14 +6,14 @@ class Node
 {
 public:
     string info;
-    Node *leftchilde;
+    Node *leftchild;
     Node *rightchild;
 
     //constructor for the node class
     Node(string i, Node *l, Node *r)
     {
         info = i;
-        leftchilde = l;
+        leftchild = l;
         rightchild = r;
     }
 };
@@ -34,7 +34,7 @@ public:
         Node *newNode = new Node(element, NULL, NULL);
 
         newNode->info = element;
-        newNode->leftchilde = NULL;
+        newNode->leftchild = NULL;
         newNode->rightchild = NULL;
 
         Node *parent = NULL;
@@ -57,5 +57,26 @@ public:
             parent->rightchild = newNode;
         }
     }
+
+    //this function searches the current node of the specified node
+    //as well as the current node of its parent
+
+    void search(string element, Node *&parent, Node *&currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+
+        while ((currentNode != NULL)&&
+               (currentNode->info != element))
+        {
+            parent = currentNode;
+
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
     
+
 }
